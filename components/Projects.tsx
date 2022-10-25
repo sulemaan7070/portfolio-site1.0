888cca49df217fe650eef385edda03a512053228
sulemaan7070 <sulemaan7070@gmail.com>
2022-10-25 12:08:01 +0530

    build version

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -18,7 +18,7 @@ function Projects({}: Props) {
       >
         {projects.map((projects, i) => {
           return (
-            <div className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen">
+            <div key ={i}className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen">
               <motion.img
                 initial={{
                   y: -300,
